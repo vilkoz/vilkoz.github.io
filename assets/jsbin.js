4d309710984dd62b0e0e528dfdf2cd35b7bdea0a
vilkoz <vilko.a@gmail.com>
2016-11-17 00:43:31 +0200

    upd site

---
+++ assets/jsbin.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(
+	function($){
+		//open-close submenu on mobile
+		$('.cd-main-nav').on('click', function(event){
+			if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
+		});
+	});
+
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top - 50
+				}, 800);
+				return false;
+			}
+		}
+	});
+
+	//SCROLLSPY by mekwall
+	// Cache selectors
+	var lastId,
+		topMenu = $("#top-menu"),
+		topMenuHeight = topMenu.outerHeight()+15,
+		// All list items
+		menuItems = topMenu.find("a"),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function(){
+			var item = $($(this).attr("href"));
+			if (item.length) { return item; }
+		});
+
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	/*	menuItems.click(function(e){
+		var href = $(this).attr("href"),
+			offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+		$('html, body').stop().animate({ 
+			scrollTop: offsetTop
+		}, 800);
+		e.preventDefault();
+	});*/
+
+	// Bind to scroll
+	$(window).scroll(function(){
+		// Get container scroll position
+		var fromTop = $(this).scrollTop()+topMenuHeight;
+
+		// Get id of current scroll item
+		var cur = scrollItems.map(function(){
+			if ($(this).offset().top < fromTop)
+				return this;
+		});
+		// Get the id of the current element
+		cur = cur[cur.length-1];
+		var id = cur && cur.length ? cur[0].id : "";
+
+		if (lastId !== id) {
+			lastId = id;
+			// Set/remove active class
+			menuItems
+				.parent().removeClass("active")
+				.end().filter("[href='#"+id+"']").parent().addClass("active");
+		}                   
+	});
+});
